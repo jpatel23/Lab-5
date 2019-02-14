38e9873b036e4496cb94ac1f7df72af6b1d27c64
Jaimin Patel <jaiminabpatel@ou.edu>
2019-02-14 10:46:09 -0600

    Foundation

---
+++ Todo.java
@@ -0,0 +1,18 @@
+public class Todo {/*
+Lab Section_________: 11
+Last Modified_______: 2019-02-14
+OU ID_______________: 113411744
+OU EMAIL____________: jaiminabpatel@ou.edu
+
+Tasks:
+1. 
+2. XXX
+
+Estimated Time (minutes):
+1. XXX
+2. XXx
+
+Actual Time (minutes):
+1. XXX
+2. XXX
+*/}
