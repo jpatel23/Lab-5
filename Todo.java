91bc4f2447eb51761f5dfaa9f59f6472e0019e67
Jaimin Patel <jaiminabpatel@ou.edu>
2019-02-14 11:04:25 -0600

    Finished listing all tasks and estimated times

--- Todo.java
+++ Todo.java
@@ -5,14 +5,59 @@ OU ID_______________: 113411744
 OU EMAIL____________: jaiminabpatel@ou.edu
 
 Tasks:
-1. 
-2. XXX
+1. Complete calculateTwoTokens method
+2. Complete calculateThreeTokens method
+3. Complete execute method
+4. Complete parseAndExecute method
+5. Complete calculateTwoTokensTestValidHalve
+6. Complete calculateTwoTokensTestInvalidCommand
+7. Complete calculateThreeTokensTestValidAdd
+8. Complete calculateThreeTokensTestValidSubtract
+9. Complete calculateThreeTokensTestValidDivide
+10. Complete calculateThreeTokensTestInvalidCommand
+11. Complete executeTestValidQuit
+12. Complete executeTestValidThreeTokens
+13. Complete part 1 of executeTestInvalidTokenLength
+14. Complete part 2 of executeTestInvalidTokenLength
+15. Complete parseAndExecuteTestDivideByZero
+16. Complete parseAndExecuteTestInvalidNumber
+17. Complete parseAndExecuteTestInvalidTokenLength
 
 Estimated Time (minutes):
-1. XXX
-2. XXx
+1. 5
+2. 5
+3. 3
+4. 5
+5. 5
+6. 5
+7. 10
+8. 10
+9. 5
+10. 5
+11. 5
+12. 5
+13. 5
+14. 5
+15. 5
+16. 5
+17. 5
 
 Actual Time (minutes):
-1. XXX
-2. XXX
+1. 
+2. 
+3. 
+4. 
+5. 
+6. 
+7. 
+8. 
+9. 
+10. 
+11. 
+12. 
+13. 
+14. 
+15. 
+16. 
+17. 
 */}
